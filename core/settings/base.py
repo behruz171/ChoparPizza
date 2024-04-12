2caabb5d6d1650f9890295887faaf2cdf99a313c
behruz171 <behruzzo662@gmail.com>
2024-04-12 13:11:30 +0500

    nimaidr

--- core/settings/base.py
+++ core/settings/base.py
@@ -19,17 +19,19 @@ from core.jazzmin_conf import *  # noqa
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 # READING ENV
-env = environ.Env()
-env.read_env(".env")
+# env = environ.Env()
+# env.read_env(".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
+# SECRET_KEY = env.str("SECRET_KEY")
+SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG")
+# DEBUG = env.bool("DEBUG")
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -173,14 +175,14 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
-        "LOCATION": f"{env.str('REDIS_URL', 'redis://localhost:6379/0')}",
+        "LOCATION": "redis://localhost:6379/0",
         "KEY_PREFIX": "boilerplate",  # todo: you must change this with your project name or something else
     }
 }
 
 # CELERY CONFIGURATION
-CELERY_BROKER_URL = env.str("CELERY_BROKER_URL", "redis://localhost:6379")
-CELERY_RESULT_BACKEND = env.str("CELERY_BROKER_URL", "redis://localhost:6379")
+CELERY_BROKER_URL = "redis://localhost:6379"
+CELERY_RESULT_BACKEND ="redis://localhost:6379"
 
 CELERY_TIMEZONE = "Asia/Tashkent"
 
